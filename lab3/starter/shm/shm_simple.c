9c8d11c1b8d4b135e15ad8fbd60da81847dd0497
Y. Huang <yqihuang@gmail.com>
2019-06-17 22:59:12 -0400

    adding sys. call return value check

--- lab3/starter/shm/shm_simple.c
+++ lab3/starter/shm/shm_simple.c
@@ -16,8 +16,11 @@
 int g_val = 0;
 
 int main( int argc, char** argv ) {
-    int shmid = shmget(IPC_PRIVATE, SHM_SIZE, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
     pid_t cpid = 0;
+    int shmid = shmget(IPC_PRIVATE, SHM_SIZE, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+    if ( shmid == -1 ){
+        perror("shmget");
+    }
 
     printf("Parent and child processes cannot use global variables to communicate!\n");
     printf("Because they do not share the same address space. Global variable only works within the same address space.\n");
@@ -26,20 +29,38 @@ int main( int argc, char** argv ) {
     if ( cpid > 0 ) { /* Parent */
         void *buf = shmat(shmid, NULL, 0);
 
+        if ( buf == (void *) -1 ) {
+            perror("shmat");
+            abort();
+        }
+
         g_val = 100;
         printf("After fork: parent (pid=%d) sees g_val = %d\n", getpid(), g_val);
         waitpid(cpid, NULL, 0);
         printf("They can use shared memory to communicate.\n");
         printf("Parent received a message from a child:  %s.\n", (char*) buf);
-        shmdt(buf);
-        shmctl(shmid, IPC_RMID, NULL);
+        if ( shmdt(buf) != 0 ) {
+            perror("shmdt");
+            abort();
+        }
+        if ( shmctl(shmid, IPC_RMID, NULL) == -1 ) {
+            perror("shmctl");
+            abort();
+        }
     } else if ( cpid == 0 ) { /* Child */
         void* buf = shmat( shmid, NULL, 0 );
+        if ( buf == (void *) -1 ) {
+            perror("shmat");
+            abort();
+        }
 
         printf("After fork: child (pid=%d) sees g_val = %d\n", getpid(), g_val);
         memset(buf, 0, SHM_SIZE);
         sprintf(buf, "I am the child (pid=%d)", getpid());
-        shmdt(buf);
+        if ( shmdt(buf) != 0 ) {
+            perror("shmdt");
+            abort();
+        }
     } else {
         perror("fork");
         abort();
